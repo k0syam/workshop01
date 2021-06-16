0ec7fed41b6e9df86c18b75edb87e202f2803f8f
k0syam <43576806+k0syam@users.noreply.github.com>
2021-06-16 22:40:27 +0900

    shuusei

--- dai03shou/code3.6.py
+++ dai03shou/code3.6.py
@@ -3,10 +3,10 @@ a = [int(input) for i in range(N)]
 
 exist = False
 
-for bit in range(int(bin(1) << bin(N))):
+for bit in range(1 << bin(N)):
     sum_num = 0
     for i in range(N):
-        if bin(bit) & (bin(1) << bin(i))):
+        if bit & (1 << i):
             sum_num += a[i]
         if sum_num == W:
             exist = True
