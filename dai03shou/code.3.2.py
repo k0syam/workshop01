cc87866d3dea69212639dd5aa714555a84cf73a9
k0syam <43576806+k0syam@users.noreply.github.com>
2021-06-16 22:28:49 +0900

    dai03shou

---
+++ dai03shou/code.3.2.py
@@ -0,0 +1,11 @@
+N, v = [int(s) for s in input().split()]
+a = [int(input) for i in range(N)]
+
+found_id = -1
+
+for i in range(N):
+    if a[i] == v:
+        found_id = i
+        break
+
+print(found_id)
