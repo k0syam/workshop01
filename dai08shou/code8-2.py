b0d6d781c49549b4d60efeae62010aab3d3eb49a
k0syam <43576806+k0syam@users.noreply.github.com>
2021-07-15 22:05:24 +0900

    dai08-09shou

---
+++ dai08shou/code8-2.py
@@ -0,0 +1,5 @@
+# Refering tHe snippet in https://engineeringnote.hateblo.jp/entry/python/algorithm-and-data-structures/linked_list
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
