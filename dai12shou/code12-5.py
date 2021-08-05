5fd0d4ef36d6d35662a88a6eb7e6ac3b7c30e358
naoshige314 <85681749+naoshige314@users.noreply.github.com>
2021-08-06 01:18:07 +0900

    fushimi_12-5
    fushimi_12-5_update

--- dai12shou/code12-5.py
+++ dai12shou/code12-5.py
@@ -16,7 +16,8 @@ def BucketSort(a):
     n_less[0]=num[0]#これいらないの？
     for v in range(1,MAX):
         n_less[v]=n_less[v-1] + num[v]
-    
+    print(num[:15])
+    print(n_less[:15])
     #ソートしたもの
     a2=[0]*n
     for i in range(n-1,-1,-1):
@@ -25,13 +26,14 @@ def BucketSort(a):
         a2[n_less[a[i]]]=a[i]
     
     #print(a2)
+    #a=a2 だとaが更新されない
     for i in range(n):
         a[i]=a2[i]
 
 
 if __name__ == '__main__':
 
-    a=[4,1,3,5,2,8,7,12,3,6]
+    a=[4,1,3,5,2,8,7,0,0,12,3,6]
     
     n=len(a)
 
