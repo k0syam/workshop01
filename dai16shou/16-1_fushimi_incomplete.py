78f58ac086cbfb5fc19b1b1bf38f0735e8ec61d7
naoshige314 <85681749+naoshige314@users.noreply.github.com>
2021-09-02 22:35:19 +0900

    aaa
    incomplete

---
+++ dai16shou/16-1_fushimi_incomplete.py
@@ -0,0 +1,95 @@
+class Graph:
+    def __init__(self,n):
+        self.li=[[] for _ in range(n)]
+
+    
+    class Edge:
+        def __init__(self,r,f,t,c):
+            self.rev=r
+            self.fr=f
+            self.to=t
+            self.cap=c
+    
+    #頂点数取得
+    def size(self):
+        return len(self.li)
+    
+    #逆辺取得
+    def redge(self,e):
+        return self.li[e.to][e.rev]
+    
+    #フローを流す
+    def run_flow(self,e,f):
+        e.cap -= f
+        self.redge(e).cap += f
+    
+    #頂点frから頂点toへ容量capの辺を張る
+    #逆辺も容量0で張っておく
+    def addedge(self,fr,to,cap):
+        self.fromrev=int(len(self.li[fr]))
+        self.torev=int(len(self.li[to]))
+        self.li[fr].append(self.Edge(self.torev,fr,to,cap))
+        self.li[to].append(self.Edge(self.fromrev,to,fr,0))
+
+class FordFulkerson:
+    def __init__(self):
+        self.INF=1<<30
+        self.seen=[]
+    
+    #dfsでs-tパスを見つける
+    #帰り値はパスの最小値
+
+    def fodfs(self,G,v,t,f):
+        if v == t:
+            return f
+        
+        self.seen[v]=True
+        for e in G.li:
+            if self.seen[e.to]:
+                continue
+            if e.cap==0:
+                continue
+
+            #探す
+            flow=self.fodfs(G,e.to,t,min(f,e.cap))
+
+            if flow==0:
+                continue
+            
+            #辺と逆辺の更新
+            G.run_flow(e,flow)
+
+            return flow
+        
+        return 0
+    
+    #s-t間の最大流量を求める
+    #リターン時のGは残余グラフになる
+    def solve(self,G,s,t):
+        res=0
+
+        while True:
+            self.seen=[False]*len(G.li)
+            self.flow=self.fodfs(G,s,t,self.INF)
+
+            if self.flow==0:
+                return res
+            res += self.flow
+        
+        return 0
+
+if __name__ == '__main__':
+    n,m=map(int,input().split())
+    G=Graph(n)
+    for i in range(m):
+        u,v,c=map(int,input().split())
+
+        G.addedge(u,v,c)
+    
+    ff=FordFulkerson()
+    s,t=0,n-1
+
+    print(ff.solve(G,s,t))
+
+
+
