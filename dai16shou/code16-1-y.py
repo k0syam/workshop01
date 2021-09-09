f696cfd11a1660b663a3c81ed60ddbe64024b6c5
k0syam <43576806+k0syam@users.noreply.github.com>
2021-09-09 21:39:19 +0900

    code implementation done
    desu

--- dai16shou/code16-1-y.py
+++ dai16shou/code16-1-y.py
@@ -8,7 +8,7 @@ class Edge:
         self.cap = c 
 
 class Graph:
-    def __init__(self, N, edges):
+    def __init__(self, N):
         self.N = N
         self.neilist = [[] for i in range(N)]
     def redge(self, edge):
@@ -23,7 +23,8 @@ class Graph:
         self.redge(edge).cap += f
 
 class FordFulkerson:
-    def __init__(self, ):
+    def __init__(self):
+        self.INF = 1 << 30
         self.seen = []
     def fodfs(self, Graph, v, t, f):
         if v == t:
@@ -31,16 +32,49 @@ class FordFulkerson:
         self.seen[v] = True
         for edgelist in Graph.neilist:
             for e in edgelist:
-                if seen[e.to]:
+                if self.seen[e.nto]:
                     continue
                 if e.cap == 0:
                     continue
-                flow = self.fodfs(Graph, e.to, t, min(f, e.cap))
+                flow = self.fodfs(Graph, e.nto, t, min(f, e.cap))
                 if flow == 0:
                     continue
                 Graph.run_flow(e, flow)
                 return flow
         return 0
     def solve(self, Graph, s, t):
-        pass
+        res = 0
+        while True:
+            print("DFS start")
+            self.seen = [False for _ in range(Graph.N)]
+            flow = self.fodfs(Graph, s, t, self.INF)
+            if flow == 0:
+                return res
+            res += flow
+            for graph_edges in Graph.neilist:
+                for graph_edge in graph_edges:
+                    print(graph_edge.nfrom, graph_edge.nto, graph_edge.cap)
+        return 0
 
+N = 6
+M = 9
+edges = [
+    [0, 1, 5],
+    [0, 3, 5],
+    [1, 2, 4],
+    [1, 3, 37],
+    [2, 4, 56],
+    [2, 5, 9],
+    [3, 2, 3],
+    [3, 4, 9],
+    [4, 5, 2]
+]
+
+graph = Graph(N)
+for edge in edges:
+    graph.addedge(edge[0], edge[1], edge[2])
+# print(graph.neilist)
+ff = FordFulkerson()
+s = 0
+t = N-1
+print(ff.solve(graph, s, t))
